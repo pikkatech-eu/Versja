1b9918987117aa751c9d821a56ae423cfb12c308
Stanislav <stanislav@pikkatech.eu>
2026-05-21 09:16:41 +0200

    Logger removed

--- Source/Versja/Program.cs
+++ Source/Versja/Program.cs
@@ -7,7 +7,6 @@
 * Copyright:    pikkatech.eu (www.pikkatech.eu)                                    *
 ***********************************************************************************/
 
-using Factotum.Logging;
 
 namespace Versja
 {
@@ -24,43 +23,26 @@ namespace Versja
 								$"CMD={Environment.CommandLine}\n"
 							);
 
-			Logger.Open("Versja");
-
-			Logger.Debug($"args.Length={args.Length}");
-
-			for (int i = 0; i < args.Length; i++)
-			{
-				Logger.Debug($"i = {i}; arg[i]={args[i]}");
-			}
 
 			if (args.Length == 2)
 			{
 				string projectFileName	= args[0];
 				string action			= args[1];
 
-				Logger.Debug($"projectFileName={projectFileName}");
-				Logger.Debug($"action={action}");
-
 				if (action == "increment")
 				{
-					Logger.Info("action = increment");
-
 					Incrementer incrementer = new Incrementer();
 					incrementer.IncrementVersion(projectFileName);
 					return;
 				}
 				else if (action == "configure")
 				{
-					Logger.Info("action = configure");
-
 					Configurator configurator = new Configurator();
 					configurator.Configure(projectFileName);
 					return;
 				}
 				else
 				{
-					Logger.Info("action = default");
-
 					Console.WriteLine("stupid");
 
 					return;
@@ -68,8 +50,6 @@ namespace Versja
 			}
 			else
 			{
-				Logger.Info("wrong number of arguments");
-
 				Console.WriteLine("Versja: Tool to semi-automated increment versions of VS projects.");
 				Console.WriteLine("Usage: versja.exe FULL_PROJECT_FILE_NAME ACTION");
 				Console.WriteLine("FULL_PROJECT_FILE_NAME: full path to the .csproj file");
